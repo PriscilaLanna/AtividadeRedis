d15d265a770b2dc50d9997057896ec60a7c0ae77
logonrmlocal <logonrmlocal@L2201MICRO34>
2018-12-03 22:27:07 -0200

    Fila

---
+++ Queue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=trezenet;AccountKey=AXwUsljgM169Q3c9IvcunCdagOXypuVtbaSs/mMmPCrPuMADu9rW7BYeEAE/B5Qm5v9sM956wpbBjpJYoj/8UQ==;EndpointSuffix=core.windows.net");
+            var queueClient = account.CreateCloudQueueClient();
+
+           var queue = queueClient.GetQueueReference("330845");
+            queue.CreateIfNotExistsAsync().Wait();
+
+            Console.WriteLine("Digite uma mensagem");
+            var msg = Console.ReadLine();
+
+            queue.AddMessageAsync(new CloudQueueMessage(msg));
+
+            var messages = queue.GetMessagesAsync(10).Result;
+            foreach(var m in messages)
+            {
+                Console.WriteLine($"Mensagem:{m.AsString}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
